be9c5a38a62f6046518411f2e1c83ffba96f75c5
tanzim <syedtanzimwajih@gmail.com>
2026-06-21 18:43:12 +0530

    feat: add useTypingTest hook with input handling and reset logic

---
+++ src/hooks/useTypingTest.js
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+import { calculateAccuracy, calculateWPM } from '../utils/typingStats';
+import { getRandomText } from '../utils/words';
+
+
+function useTypingTest() {
+
+    const [practiceText, setPracticeText] = useState(getRandomText());
+    const [typedText, setTypedText] = useState('');
+    const [startTime, setStartTime] = useState(null);
+    const [endTime, setEndTime] = useState(null);
+
+
+    function handleInput(newValue) {
+        if (startTime == null) {
+            setStartTime(Date.now());
+        }
+
+        setTypedText(newValue);
+
+        if (newValue.length == practiceText.length) {
+            setEndTime(Date.now());
+        }
+    }
+
+    function reset() {
+        setPracticeText(getRandomText());
+        setTypedText('');
+        setStartTime(null);
+        setEndTime(null);
+    }
+
+    const accuracy = calculateAccuracy(practiceText, typedText);
+    const wpm = calculateWPM(typedText, startTime, endTime);
+
+    return {practiceText, typedText, handleInput, reset, accuracy, wpm}
+}
